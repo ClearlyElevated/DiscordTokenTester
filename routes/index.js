d6e90181cc89126eb0b18a92d69c2b3c15f0ca1a
Edouard Courty <edouard.courty2@gmail.com>
2018-12-19 21:17:05 +0100

    Stable version. Backend script for gathering informations not started yet.

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,15 @@ var router = express.Router()
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', {
-    title: 'Token Tester',
+    title: 'Discord Token Tester',
     stylesheets: ['style.css'],
     scripts: ['app.js']
   })
+})
+
+/* POST home page */
+router.post('/', (req, res, next) => {
+  console.log(req.body)
 })
 
 module.exports = router
