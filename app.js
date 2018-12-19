82baea0f618efd4000015f9e2a8ced20cb2a169b
Edouard Courty <edouard.courty2@gmail.com>
2018-12-19 22:01:28 +0100

    Small changed

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
+// REQUIRING DEPENDENCIES
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
+// REQUIRING ALL THE ROUTERS
 var indexRouter = require('./routes/index')
+var apiRouter = require('./routes/api')
 
+// Creating app
 var app = express()
 
-// view engine setup
+// Setting-up Handlebars as view engine (rendering engine)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
+// Middleware declarations
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Declaring the use of wanted routes and attached routers
 app.use('/', indexRouter)
+app.use('/api', apiRouter)
 
-// catch 404 and forward to error handler
+// Send 404 Error if route does not exists
 app.use((req, res, next) => {
   next(createError(404))
 })
 
-// error handler
+// Error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500)
   res.render('error')
 })
