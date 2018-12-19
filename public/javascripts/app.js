17e3d77cf4075c442f43e3cf226eb78cceaec729
Edouard Courty <edouard.courty2@gmail.com>
2018-12-19 21:45:52 +0100

    Setting-up ajax form transmission

---
+++ public/javascripts/app.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  // Processing the form
+  $('form').submit((event) => {
+    var formData = {
+      'token': $('input[name=token]').val()
+    }
+    // Processing the ajax request
+    $.ajax({
+      type: 'POST',
+      url: '/',
+      data: formData,
+      dataType: 'json',
+      encode: true
+    })
+    .done((data) => {
+      console.log(data);
+    })
+    event.preventDefault()
+  })
+})
