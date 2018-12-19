d98c45cff53e31b6c4b6f55ce3ed3009e7a0a595
Edouard Courty <edouard.courty2@gmail.com>
2018-12-19 18:07:11 +0100

    Setting-up css / scripts handling

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', {
-    title: 'Express',
-    stylesheets: ['style.css']
+    title: 'Token Tester',
+    stylesheets: ['style.css'],
+    scripts: ['app.js']
   })
 })
 
