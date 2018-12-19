8e31bd296ebfa14ae061adffb007b118e4684e5b
Edouard Courty <edouard.courty2@gmail.com>
2018-12-19 22:01:53 +0100

    Small changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// REQUIRING DEPENDENCIES
+// Requiring dependencies
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-// REQUIRING ALL THE ROUTERS
+// Requiring routers
 var indexRouter = require('./routes/index')
 var apiRouter = require('./routes/api')
 
