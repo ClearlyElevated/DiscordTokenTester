0613e1ad11df2f08460c8469af06dc8227fa11ec
Edouard Courty <edouard.courty2@gmail.com>
2018-12-20 23:18:19 +0100

    Need css. FAST

--- public/javascripts/handler.js
+++ public/javascripts/handler.js
@@ -19,17 +19,28 @@ $(document).ready(() => {
   })
 
   // Processing reset
-  $("#resetButton").click(function() {
-    $("#botInfo").fadeOut(1000, function() {
+  $('#resetButton').click(function() {
+    $('#botInfo').fadeOut(400, function() {
       $(this).remove()
+      $('main form input[type=submit]').prop('disabled', false)
     })
   })
 })
 
 var HTMLincrement = (client) => {
-  $("main form input[type=submit]").prop("disabled", true)
-  $("#tester").append('<div class="w90 bg-blue flex column space-between flex-07" id="botInfo"></div>')
+  $('main form input[type=submit]').prop('disabled', true)
+  $('#tester').append('<div class="w90 bg-blue flex column space-between flex-07" id="botInfo"></div>')
+  $('#botInfo').append(`<img src="${client.user.avatarURL}" height=100 width=100>`)
   createFields(3)
+
+  $('#infoLine1').append(`<div class="container middle">TAG : ${client.user.tag}</div>`)
+  $('#infoLine1').append(`<div class="container middle">ID : ${client.user.id}</div>`)
+
+  $('#infoLine2').append(`<div class="container middle">Guilds : ${client.guilds.length}</div>`)
+  $('#infoLine2').append(`<div class="container middle">Channels : ${client.channels.length}</div>`)
+
+  $('#infoLine3').append(`<div class="container middle">Avatar : ${client.user.avatarURL}</div>`)
+  $('#infoLine3').append(`<div class="container middle">Users : ${client.users.length}</div>`)
 }
 
 // Utils
@@ -37,6 +48,6 @@ function repeat(fn, times) {for(var i = 0; i < times; i++) fn()}
 
 function createFields(n) {
   for(var i = 0; i < n; i++) {
-    $("#botInfo").append(`<div class="bg-red flex-03 infoLine" id="infoLine${i+1}"></div>`)
+    $('#botInfo').append(`<div class="bg-red flex space-between flex-03 infoLine" id="infoLine${i+1}"></div>`)
   }
 }
