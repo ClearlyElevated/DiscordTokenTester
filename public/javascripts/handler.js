b3d4e329ce4e2d65b92c9ccfd2cc76d9b29bee09
Edouard Courty <edouard.courty2@gmail.com>
2018-12-20 21:25:04 +0100

    Added reset button on ajax form

--- public/javascripts/handler.js
+++ public/javascripts/handler.js
@@ -17,12 +17,26 @@ $(document).ready(() => {
     })
     event.preventDefault()
   })
+
+  // Processing reset
+  $("#resetButton").click(function() {
+    $("#botInfo").fadeOut(1000, function() {
+      $(this).remove()
+    })
+  })
 })
 
 var HTMLincrement = (client) => {
   $("main form input[type=submit]").prop("disabled", true)
   $("#tester").append('<div class="w90 bg-blue flex column space-between flex-07" id="botInfo"></div>')
-  $("#botInfo").append(`<div class="bg-red flex-03" id="infoLine1"></div>`)
-  $("#botInfo").append(`<div class="bg-red flex-03" id="infoLine2"></div>`)
-  $("#botInfo").append(`<div class="bg-red flex-03" id="infoLine3"></div>`)
+  createFields(3)
+}
+
+// Utils
+function repeat(fn, times) {for(var i = 0; i < times; i++) fn()}
+
+function createFields(n) {
+  for(var i = 0; i < n; i++) {
+    $("#botInfo").append(`<div class="bg-red flex-03 infoLine" id="infoLine${i+1}"></div>`)
+  }
 }
