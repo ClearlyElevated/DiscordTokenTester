68408708d9415ec7d80110f8e972e4b14af41078
Edouard Courty <edouard.courty2@gmail.com>
2018-12-21 12:34:09 +0100

    Cleared css

--- public/javascripts/handler.js
+++ public/javascripts/handler.js
@@ -29,18 +29,18 @@ $(document).ready(() => {
 
 var HTMLincrement = (client) => {
   $('main form input[type=submit]').prop('disabled', true)
-  $('#tester').append('<div class="w90 bg-blue flex column space-between flex-07" id="botInfo"></div>')
-  $('#botInfo').append(`<img src="${client.user.avatarURL}" height=100 width=100>`)
+  $('#tester').append('<div class="w90 flex column space-between flex-07" id="botInfo"></div>')
+  $('#botInfo').append(`<img src="${client.user.avatarURL}" id="avatarPicture">`)
   createFields(3)
 
-  $('#infoLine1').append(`<div class="container middle">TAG : ${client.user.tag}</div>`)
-  $('#infoLine1').append(`<div class="container middle">ID : ${client.user.id}</div>`)
+  $('#infoLine1').append(`<div class="botValue"><span class="prop">TAG</span><span class="propVal">${client.user.tag}</span></div>`)
+  $('#infoLine1').append(`<div class="botValue"><span class="prop">ID</span><span class="propVal">${client.user.id}</span></div>`)
 
-  $('#infoLine2').append(`<div class="container middle">Guilds : ${client.guilds.length}</div>`)
-  $('#infoLine2').append(`<div class="container middle">Channels : ${client.channels.length}</div>`)
+  $('#infoLine2').append(`<div class="botValue"><span class="prop">Guilds</span><span class="propVal">${client.guilds.length}</span></div>`)
+  $('#infoLine2').append(`<div class="botValue"><span class="prop">Users</span><span class="propVal">${client.users.length}</span></div>`)
+  $('#infoLine2').append(`<div class="botValue"><span class="prop">Channels</span><span class="propVal">${client.channels.length}</span></div>`)
 
-  $('#infoLine3').append(`<div class="container middle">Avatar : ${client.user.avatarURL}</div>`)
-  $('#infoLine3').append(`<div class="container middle">Users : ${client.users.length}</div>`)
+  $('#infoLine3').append(`<div class="botValue"><span class="prop">Avatar</span><span class="propVal">${client.user.avatarURL}</span></div>`)
 }
 
 // Utils
@@ -48,6 +48,6 @@ function repeat(fn, times) {for(var i = 0; i < times; i++) fn()}
 
 function createFields(n) {
   for(var i = 0; i < n; i++) {
-    $('#botInfo').append(`<div class="bg-red flex space-between flex-03 infoLine" id="infoLine${i+1}"></div>`)
+    $('#botInfo').append(`<div class="flex space-between flex-03 infoLine" id="infoLine${i+1}"></div>`)
   }
 }
